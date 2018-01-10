2a20320966fdd354b9ed2b348820063650996f51
Work <work@stuarth-macbook-15.local>
2018-01-10 17:21:54 +0000

    Started to look at asymmetric encryption

--- js/browsercrypto.js
+++ js/browsercrypto.js
@@ -1,3 +1,49 @@
+// Hex of DER encoded pubkey
+pubkey = '30820122300d06092a864886f70d01010105000382010f003082010a0282\
+010100be732432e866522d7af3e14c4933ee55b6b14925fab193429dbdad\
+3381e1f1253ddefef88e23967b8fc9b9902d53dbfaa8857f52e315889c1e\
+0c31eb09d1881c95ba938e92c0b8a089792f2f16193a120aca67e0b433ab\
+ad74ddb7254009b6a2bedf9c035d13dc16f70fae87ea06e5141219085b97\
+7e99308016030a3979cfe823b6f3d2599983ee33f22ac0b177616f0aee84\
+155faa46470a472a9ac88dc2015132b9514b6724d29b2332a5a1df9bd8bd\
+d1d9fde9c6f7210025ea8c91e271520220fe973825c15a6f415927095624\
+97afb6b0e9c85690e9433b7a25ccea97b601d67e7c648a27e7155fefb0bc\
+7120616e3a376ff326161c7dac4a589c04bb430203010001';
+
+pubkeyBuffer = buffer.Buffer.from(pubkey, 'hex');
+
+// For testing purposes
+privatekey = '-----BEGIN PRIVATE KEY-----\
+MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC+cyQy6GZSLXrz\
+4UxJM+5VtrFJJfqxk0Kdva0zgeHxJT3e/viOI5Z7j8m5kC1T2/qohX9S4xWInB4M\
+MesJ0YgclbqTjpLAuKCJeS8vFhk6EgrKZ+C0M6utdN23JUAJtqK+35wDXRPcFvcP\
+rofqBuUUEhkIW5d+mTCAFgMKOXnP6CO289JZmYPuM/IqwLF3YW8K7oQVX6pGRwpH\
+KprIjcIBUTK5UUtnJNKbIzKlod+b2L3R2f3pxvchACXqjJHicVICIP6XOCXBWm9B\
+WScJViSXr7aw6chWkOlDO3olzOqXtgHWfnxkiifnFV/vsLxxIGFuOjdv8yYWHH2s\
+SlicBLtDAgMBAAECggEAO1Bq9rvwmil2AJyrgKT+1o0mm+yLLmD5v10Co2cRMmRx\
+55CrYsFsraDzp6pio5qAb5ncNLlqzHgq14t4cz9hz91GFYyy4fjebtJiQpz0UGSs\
+HavKjPUGw+gA4XhlgGD0QNQtp9KGS+aPHcAyrk1lbSHR0AuOnCwjsdCpM7cA3CIa\
+3sOi7DS2s/et5MBwPGfKfno+UcUIBV0/UcAs5eEgcLAoV3RXNyScbJaHTUv0HVPi\
+Fb3OZ14QqCc1IxtNNEjUj74rc6R9qFfyaBQTfzWwrEmb3JmrEmgQFYBKbtIW+BB4\
+/1aVUrfCUeCmo8n0ZUZNymgdjyNA0aCh4YrRQ5ITUQKBgQD508i0u4xORsyfsNmZ\
+QcQFg0IuZM9hEyPDaY7pMm+yr883sLe8MoVDuUUrceUAXfbGNy6kvqqgy1NGYX/J\
+0hZ8y3QrEC2Pn8wiDtpwHDibgZ8x5dUiTPtxgkdV/Vk77OpYG+CxQyJjiwPRXEVV\
+wC/TOK3i/nOmA3bQZ33NC5o+ewKBgQDDJ8ffbBzmreiCWDXFtVuBAXY3qdLzXj3+\
+7F0816aAxyi9+F/IB0kCa0fmEVGRYDkxhDhVgP6C7qRw7wz0BGOaEdROjo4M9V/c\
+V3ndtAu25ArfcjMXvp36UvdHEwK3ge1moVlXkyzUmz6KHirEwVYPJa7yaO8dR2tZ\
+VXJEuHi/2QKBgQDUbnh47g01NlCi7W9qORjkkyNAbepFIlBDxsuFkaSXLZWnRjZF\
+6jOVTcH6WkOel0fSbnPUFGjkh2ANNbTCuUjz3tCuGXiUaw3aXAT0VZrw6Dyk4kjk\
+fM9GSsA750zsft0aBMKAGJTyTe/2I161ttEz6zs5DKLsJH/wYEABjf8fZwKBgQCc\
+HFFlpy1DVnB8/CUfn6CwBMRVaLMH3SaIqvk2t2dI8ofj1zB/aVx84+ai4s22FhwK\
+QTNzKnntsQq4EHHzLSOj1olXwe9d7FcfgpZIxELurWMJNWgroV7sJLwMDegJdZS7\
+mWxHgsLE297eS514wROfkExvO4OzjzMPRivfTxXAmQKBgQDYh03+wZFmoyoeR2RE\
+XZ+aPTNtIL+Wycj+675aixL+xQhb98BTnlwysmV+Nb7l9RXN2TgmzQqiWz3OM4ps\
+p9O/0nUX97c2ujwf8GJxFrKg0RSyPGkOIOgcJHhaNWdert6K7ZgKScns9wMJ1xH+\
+z8s4axAxANhjeg2CCV00LPEZyg==\
+-----END PRIVATE KEY-----'
+
+privkeyBuffer = buffer.Buffer.from(privatekey, 'ascii');
+
 function genRandomHexString(size) {
     var salt = new Uint8Array(size);
 
@@ -125,3 +171,62 @@ function symmetricDecrypt(plaintext, key, iv, callback) {
 
 }
 
+
+
+//For Testing purposes
+plaintextBuffer = buffer.Buffer.from("test", 'ascii');
+ivBuffer = buffer.Buffer.from("6cc22530b5ccef1e74d065fc7d754532", 'hex');
+                               
+/*
+Encrypt with public key
+
+*/
+function asymmetricEncrypt(plaintext, publickey, iv, callback) {
+
+    keyAlgo = {name: "RSA-OAEP", hash: "SHA-256"};
+    encryptAlgo = {name: "RSA-OAEP"};
+
+    window.crypto.subtle.importKey(
+        'spki',
+        publickey,
+        keyAlgo,
+        true,
+        ['encrypt']
+    ).then(function (key) {
+        console.log(key);
+        return window.crypto.subtle.encrypt(
+            encryptAlgo,
+            key,
+            plaintext
+        )
+    }).then(function (ciphertext) {
+        callback(ciphertext);
+    });
+
+}
+
+/*
+Decrypt with private key
+
+*/
+function asymmetricDecrypt(ciphertext, privatekey, iv, callback) {
+
+    keyAlgo = {name: "RSA-OAEP", hash: "SHA-256"};
+    decryptAlgo = {name: "RSA-OAEP"};
+
+    window.crypto.subtle.importKey(
+        'pkcs8',
+        privatekey,
+        keyAlgo,
+        true,
+        ['decrypt']
+    ).then(function (key) {
+        return window.crypto.subtle.decrypt(
+            decryptAlgo,
+            key,
+            cipertext
+        )
+    }).then(function (plaintext) {
+        console.log(plaintext);
+    });
+}
